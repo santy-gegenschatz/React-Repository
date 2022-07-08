1fad1572d33155701154ce7fa67c6149aef4b44a
santy-gegenschatz <gegenschatz.santiago@gmail.com>
2022-07-08 00:34:52 -0300

    Mejoras en los componentes

--- Entregable3/entregable-3/src/containers/ItemListContainer/ItemListContainer.jsx
+++ Entregable3/entregable-3/src/containers/ItemListContainer/ItemListContainer.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import './ItemListContainer.css'
 
 const ItemListContainer = ({greeting}) => {
   return (
-    <div className = "div-principal">
-        <p> Este es un componente contenedor que muestra un saludo que llega por un prop. </p>
-        <p> {greeting} </p>
+    <div className = 'div-principal'>
+        <div className = 'div-secundario'>
+            <p> Este es un componente contenedor que muestra un saludo que llega por un prop. </p>
+            <p> {greeting} </p>
+        </div>
     </div>
   )
 }
