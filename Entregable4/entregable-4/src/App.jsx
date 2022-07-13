ea212c002abf64d95ddf226c2b57a57ccdbbbacf
santy-gegenschatz <gegenschatz.santiago@gmail.com>
2022-07-13 17:38:02 -0300

    Uso de Hooks

--- Entregable4/entregable-4/src/App.jsx
+++ Entregable4/entregable-4/src/App.jsx
@@ -1,6 +1,7 @@
 import Menu from './components/Menu/Menu';
 import './App.css';
 import AñadirProducto from './components/AñadirProducto/AñadirProducto';
+import ItemListCointainer from './components/ItemListContainer/ItemListCointainer';
 
 function App() {
   return (
@@ -9,6 +10,8 @@ function App() {
       <p> Entregable 4</p>
       <p> Generar un componente que permita añadir productos al carrito</p>
       <AñadirProducto start = { 0 }/>
+      <p> Acá va un item list container</p>
+      <ItemListCointainer greeting = "Hola, soy ItemListContainer" />
     </div>
   );
 }
