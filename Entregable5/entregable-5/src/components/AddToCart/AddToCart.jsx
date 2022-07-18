ade9eec275120b66e0710ae3f92bf949ceba4f12
santy-gegenschatz <gegenschatz.santiago@gmail.com>
2022-07-18 14:43:31 -0300

    Item Detail Añadido

---
+++ Entregable5/entregable-5/src/components/AddToCart/AddToCart.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { useState } from 'react'
+import './AddToCart.css'
+
+const AddToCart = () => {
+
+    const [itemQuantity, setItemQuantity] = useState(0);
+
+    function aumentar() {
+        setItemQuantity( itemQuantity + 1);
+    }
+
+    function disminuir() {
+        setItemQuantity( itemQuantity - 1);
+    }
+
+
+
+    return (
+        <div>
+            This is the AddToCart Component
+            <p> Select the number of Items you would like to add to your cart</p>
+            <div className = 'div-key-components'>
+                <button className = 'btn btn-info' onClick = {aumentar()}> - </button>
+                <p> {itemQuantity} </p>
+                <button className = 'btn btn-info' onClick = {disminuir()}> + </button>
+            </div>
+        </div>
+    )
+}
+
+export default AddToCart
