3326e69e32e237137238b255ab83b1688d8c9fee
santy-gegenschatz <gegenschatz.santiago@gmail.com>
2022-07-22 13:35:49 -0300

    New Submenu for Categories

--- Entregable7/entregable-7/src/components/Menu/Menu.jsx
+++ Entregable7/entregable-7/src/components/Menu/Menu.jsx
@@ -14,63 +14,56 @@ const Menu = function () {
      }
     }
     return (
-        <div className = "div-menu">
-          <div className = 'center'> 
-               <div className = 'superior-menu'>
-                    <h1> Welcome to the Winter Store </h1>
-                    <ul>
-                         <li>
-                              <a href = "/inicio"> Home </a>
-                         </li>
+        <div className = "div-menu">          
+          <div className = 'superior-menu'>
+               <h1> Welcome to the Winter Store </h1>
+               <ul>
+                    <li>
+                         <a href = "/inicio"> Home </a>
+                    </li>
 
-                         <li>
-                              <a href = "/newSeason"> New Season </a>
-                         </li>
+                    <li>
+                         <a href = "/newSeason"> New Season </a>
+                    </li>
 
-                         <li>
-                              {/* Notar que en el onClick le estamos cambiando la visibilidad con una */}
-                              {/* arrow function. Si directamente usara el método de useState */}
-                              {/* Daría error, pq todo lo que esta entre {} se evalua inmediatamente */}
-                              <a onClick = {cambiarVisibilidad}> Products </a>
-                         </li>
+                    <li>
+                         {/* Notar que en el onClick le estamos cambiando la visibilidad con una */}
+                         {/* arrow function. Si directamente usara el método de useState */}
+                         {/* Daría error, pq todo lo que esta entre {} se evalua inmediatamente */}
+                         <a onClick = {cambiarVisibilidad} id = 'link-products' > Products </a>
+                    </li>
 
-                         <li>
-                              <a href = "/favs"> Favs </a>
-                         </li>
+                    <li>
+                         <a href = "/favs"> Favs </a>
+                    </li>
 
-                         <li>
-                              <a href = "/giftCards"> Gift Cards </a>
-                         </li>
-                    </ul>
-               </div>
-               {}
-               <div className = 'div-optional-products-menu'>
-               {productCategoriesVisible == true ? 
-               <div> 
-                    <ul>
-                         <li>
-                              <a href="/categories/shirts"> Shirts </a>
-                         </li>
-
-                         <li>
-                              <a href="/categories/pants"> Pants </a>
-                         </li>
-
-                         <li>
-                              <a href="/categories/jackets"> Jackets </a>
-                         </li>
-
-                         <li>
-                              <a href="/categories/accesories"> Accesories </a>
-                         </li>
-                    </ul>
-               </div>
-               :
-               <div> </div>
-               }
-               </div>
+                    <li>
+                         <a href = "/giftCards"> Gift Cards </a>
+                    </li>
+               </ul>
           </div>
+          {productCategoriesVisible == true 
+          && 
+          <div className = 'categories-menu'> 
+               <ul>
+                    <li>
+                         <a href="/categories/shirts"> Shirts </a>
+                    </li>
 
+                    <li>
+                         <a href="/categories/pants"> Pants </a>
+                    </li>
+
+                    <li>
+                         <a href="/categories/jackets"> Jackets </a>
+                    </li>
+
+                    <li>
+                         <a href="/categories/accesories"> Accesories </a>
+                    </li>
+               </ul>
+          </div>
+          }
           <div className = 'right'>
                <CartWidget items = '5'/>
           </div> 
