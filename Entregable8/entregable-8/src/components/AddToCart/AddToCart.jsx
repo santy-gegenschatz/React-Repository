89425fc671379ade1d8385d0c5d64d51337b0350
santy-gegenschatz <gegenschatz.santiago@gmail.com>
2022-07-26 22:34:57 -0300

    Entregable 8 deployed

---
+++ Entregable8/entregable-8/src/components/AddToCart/AddToCart.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { useState } from 'react'
+import './AddToCart.css'
+
+const AddToCart = () => {
+
+    const [itemQuantity, setItemQuantity] = useState(0);
+
+    function aumentar() {
+        if (itemQuantity <100) {
+            setItemQuantity( itemQuantity + 1);
+        }
+        
+    }
+
+    function disminuir() {
+        if (itemQuantity >0) {
+            setItemQuantity( itemQuantity - 1);
+        }
+        
+    }
+
+    return (
+        <div className = 'div-general'>
+            AddToCart Component
+            <p> Select the number of Items you would like to add to your cart</p>
+            <div className = 'div-key-components'>
+                <button className = 'btn btn-primary' onClick = {disminuir}> - </button>
+                <div className = 'center'>
+                    <p className = 'item-counter-label'> {itemQuantity} </p>
+                </div>
+                <button className = 'btn btn-primary' onClick = {aumentar}> + </button>
+            </div>
+            <button className = 'btn btn-primary w-75 btn-buy'> Buy Now </button>
+            <button className = 'btn btn-info w-75 btn-buy'> Add to Wishlist </button>
+        </div>
+    )
+}
+
+export default AddToCart
---
+++ Entregable8/entregable-8/src/components/Menu/Menu.jsx
@@ -0,0 +1,78 @@
+import './Menu.css';
+import CartWidget from '../CartWidget/CartWidget';
+import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+
+const Menu = function () {
+    const [productCategoriesVisible, setProductCategoriesVisible] = useState(false);
+
+    const cambiarVisibilidad = () => {
+     if (productCategoriesVisible === true) {
+          setProductCategoriesVisible(false);
+     } else {
+          setProductCategoriesVisible(true);
+     }
+    }
+    return (
+        <div className = "div-menu">          
+          <div className = 'superior-menu'>
+               <h1> Welcome to the Winter Store </h1>
+               <div className = 'ul-container'>
+                    <ul>
+                         <li>
+                              <a href = "/inicio"> Home </a>
+                         </li>
+
+                         <li>
+                              <a href = "/newSeason"> New Season </a>
+                         </li>
+
+                         <li>
+                              {/* Notar que en el onClick le estamos cambiando la visibilidad con una */}
+                              {/* arrow function. Si directamente usara el método de useState */}
+                              {/* Daría error, pq todo lo que esta entre {} se evalua inmediatamente */}
+                              <a onClick = {cambiarVisibilidad} id = 'link-products' > Products </a>
+                         </li>
+
+                         <li>
+                              <a href = "/favs"> Favs </a>
+                         </li>
+
+                         <li>
+                              <a href = "/giftCards"> Gift Cards </a>
+                         </li>
+                    </ul>
+               </div>
+          </div>
+          {productCategoriesVisible == true 
+          && 
+          <div className = 'categories-menu'> 
+               <div>
+                    <ul>
+                         <li>
+                              <a href="/categories/shirts"> Shirts </a>
+                         </li>
+
+                         <li>
+                              <a href="/categories/pants"> Pants </a>
+                         </li>
+
+                         <li>
+                              <a href="/categories/jackets"> Jackets </a>
+                         </li>
+
+                         <li>
+                              <a href="/categories/accesories"> Accesories </a>
+                         </li>
+                    </ul>
+               </div>
+          </div>
+          }
+          <div className = 'right'>
+               <CartWidget items = '5'/>
+          </div> 
+        </div>
+    );
+}
+
+export default Menu;
